cd2101897dcc261f92c8ebc304595871bbf4f692
Logan Gibson <gibsonl328@gmail.com>
2024-11-21 21:18:56 -0500

    bug: sync

--- index.js
+++ index.js
@@ -5,17 +5,21 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import './msw.polyfills';
+import {server} from './src/mocks/server';
 
-async function enableMocking() {
-  if (!__DEV__) {
-    return;
-  }
+// async function enableMocking() {
+//   if (!__DEV__) {
+//     return;
+//   }
 
-  await import('./msw.polyfills');
-  const {server} = await import('./src/mocks/server');
-  server.listen();
-}
+//   await import('./msw.polyfills');
+//   const {server} = await import('./src/mocks/server');
+//   server.listen();
+// }
 
-enableMocking().then(() => {
-  AppRegistry.registerComponent(appName, () => App);
-});
+server.listen();
+
+// enableMocking().then(() => {
+// });
+AppRegistry.registerComponent(appName, () => App);
